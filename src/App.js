80fae79461ac84e1882fc7e6b3118255effa023e
Ashab Hussan <46869907+Shadow-Chaser@users.noreply.github.com>
2021-06-23 14:54:21 +0600

    [ADDED] Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './components/Home/Home';
+import Navigator from './components/Navigator/Navigator';
+import Login from './components/Login/Login'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Admin from './components/Admin/Admin';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navigator></Navigator>
+      <Switch>
+        <Route path="/home">
+          <Home></Home>
+        </Route>
+        <Route path="/admin">
+          <Admin></Admin>
+        </Route>
+        <Route path="/login">
+          <Login></Login>
+        </Route>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
+
---
+++ src/components/Navigator/Navigator.js
@@ -0,0 +1,53 @@
+// import React from 'react';
+// import { Nav, Navbar } from 'react-bootstrap';
+
+// const Navigator = () => {
+//     return (
+//         <Navbar bg="" expand="lg">
+//             <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+//             <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//             <Navbar.Collapse id="basic-navbar-nav">
+//                 <Nav className="mr-right">
+//                     <Nav.Link href="#home">Home</Nav.Link>
+//                     <Nav.Link href="#link">Link</Nav.Link>
+//                 </Nav>
+
+//             </Navbar.Collapse>
+//         </Navbar>
+//     );
+// };
+
+// export default Navigator;
+
+
+import React from 'react';
+import { Button, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Navigator.css'
+import logo from '../../assets/logo.png'
+
+const Navigator = () => {
+    return (
+        <Navbar bg='' className='navbar-container' expand="lg">
+            <Navbar.Brand className='ml-5'><img src={logo} style={{ width: '120px' }} alt="" srcset="" /></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto mr-5">
+                    <li>
+                        <Link className='single-link' to='/home'>Home</Link>
+                    </li>
+                    <li>
+                        <Link className='single-link' to='/admin'>Admin</Link>
+                    </li>
+                    <li>
+                        <Link className='single-link' to='/login'>Login</Link>
+                    </li>
+
+                </Nav>
+
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
+
+export default Navigator;
